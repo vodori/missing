174ff287fe988c0d8fbc1e56790635401380eddf
paul.rutledge <paul.rutledge@vodori.com>
2018-07-11 20:30:17 -0500

    add locking by value

---
+++ src/missing/locks.clj
@@ -0,0 +1,40 @@
+(ns missing.locks
+  (:require [clojure.core :as clj]))
+
+
+(def ^:dynamic *locks* (atom {}))
+
+(defn- new-lock []
+  {:count 1 :ref (Object.)})
+
+(defn- inc-lock [lock]
+  (when lock (update lock :count inc)))
+
+(defn- dec-lock [lock]
+  (when lock (update lock :count dec)))
+
+(defn- inc-or-new [lock]
+  (or (inc-lock lock) (new-lock)))
+
+(defn- lease [k]
+  (get-in (swap! *locks* update k inc-or-new) [k :ref]))
+
+(defn- release [k]
+  (swap! *locks*
+         (fn [locks]
+           (when-some [entry (dec-lock (get locks k))]
+             (if (zero? (:count entry))
+               (dissoc locks k)
+               (assoc locks k entry))))))
+
+(defmacro locking [value & body]
+  `(let [v# ~value]
+     (try
+       (clj/locking (lease v#) ~@body)
+       (finally (release v#)))))
+
+(defmacro with-locks [locks & body]
+  `(binding [*locks* ~locks] ~@body))
+
+(defmacro with-own-locks [& body]
+  `(binding [*locks* (atom {})] ~@body))
---
+++ test/missing/locks_test.clj
@@ -0,0 +1,28 @@
+(ns missing.locks-test
+  (:require [clojure.test :refer :all])
+  (:require [missing.locks :as locks])
+  (:import (java.util UUID)))
+
+
+(deftest on-value-test
+  (testing "When I lock on a value, another thread will wait until I'm done."
+    (let [thing-to-mutate        (atom #{})
+          bad-mutation-scheduled (atom false)
+          value-to-lock-on       (str (UUID/randomUUID))
+          different-value        (str (UUID/randomUUID))]
+      (locks/locking value-to-lock-on
+        (future
+          (swap! bad-mutation-scheduled not)
+          (locks/locking value-to-lock-on (swap! thing-to-mutate conj :bad)))
+        (future
+          (locks/locking different-value (swap! thing-to-mutate conj :okay)))
+        (Thread/sleep 1000)
+        (is @bad-mutation-scheduled)
+        (is (contains? @thing-to-mutate :okay))
+        (is (false? (contains? @thing-to-mutate :bad)))
+        (swap! thing-to-mutate conj :good)
+        (is (contains? @thing-to-mutate :good)))
+      (Thread/sleep 1000)
+      (is (contains? @thing-to-mutate :good))
+      (is (contains? @thing-to-mutate :okay))
+      (is (contains? @thing-to-mutate :bad)))))
