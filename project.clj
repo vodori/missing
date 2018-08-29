986eaf2788ba19bd15b1380ebf5da790cf6f83f4
paul.rutledge <paul.rutledge@vodori.com>
2018-08-29 09:28:23 -0500

    Version 0.1.17

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
 
 
-(defproject com.vodori.pepper/missing "0.1.17-SNAPSHOT"
+(defproject com.vodori.pepper/missing "0.1.17"
 
   :repositories
   [["releases" {:username      :env/ARTIFACTORY_USERNAME
