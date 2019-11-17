8873e796c223831e2acd34c1a8ff3b486f0c108c
paul.rutledge <paul.rutledge@vodori.com>
2019-11-17 15:59:51 -0600

    Version 0.1.16-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.vodori/missing "0.1.15"
+(defproject com.vodori/missing "0.1.16-SNAPSHOT"
 
   :description
   "A utility library for Clojure of functions and macros that are frequently missed and recreated."
