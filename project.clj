8f3fb8b586f8b3b14bc3384e934ee009f01661ae
julia.cicale <julia.cicale@vodori.com>
2020-03-06 11:42:51 -0600

    Version 0.1.26

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.vodori/missing "0.1.26-SNAPSHOT"
+(defproject com.vodori/missing "0.1.26"
 
   :description
   "A utility library for Clojure of functions and macros that are frequently missed and recreated."
